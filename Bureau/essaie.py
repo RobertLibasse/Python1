857b1e0f372cc95e2d0bed451422bbc14d1a60e8
RobertLibasse <49407852+RobertLibasse@users.noreply.github.com>
2019-04-11 12:18:19 +0200

    Update essaie.py

--- Bureau/essaie.py
+++ Bureau/essaie.py
@@ -5,11 +5,3 @@ y = 15
 m = (x+y)/2
 
 print("moyenne égale %f" %(m))
-
-x = input("saisir la première valeur")
-y = input("saisir la deuxième valeur")
-m = (int(x) + int(y))/2
-
-#print("la moyenne de %d et %d égale %d" %(x,y,m))
-print(m)
-

afea623868ef5450cc3c27b4f29f601061863f47
RobertLibasse <49407852+RobertLibasse@users.noreply.github.com>
2019-04-11 11:44:49 +0200

    Add files via upload

---
+++ Bureau/Parite.py
@@ -0,0 +1,11 @@
+#Nombre pair et impair
+
+x = int(input("saisir la valeur"))
+
+if x %2 ==0:
+   
+    print ("Vous avez saisi un nombre pair")
+
+else: 
+
+   print ("Vous avez saisi un nombre impair")
